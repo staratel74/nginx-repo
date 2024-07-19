1482bef839254ed5c8b5aff3eb1de700499e81a4
Aleksandr <all-lla@mail.ru>
2024-07-20 00:49:41 +0500

    Update index.html v1.10.3

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline {
         DOCKER_CREDENTIALS_ID = 'dockerhub-staratel'
         REPO_URL = 'git@github.com:staratel74/nginx-repo.git'
         KUBECONFIG_CREDENTIALS_ID = 'kubeconfig-yandex'
-        TARGET_TAG = '1.10.2' // Тег, при котором будет выполняться сборка и деплой
+        TARGET_TAG = '1.10.3' // Тег, при котором будет выполняться сборка и деплой
     }
     stages {
         stage('Clone Repository') {
@@ -48,17 +48,31 @@ pipeline {
                 echo "Tag ${env.GIT_TAG} matches target tag ${env.TARGET_TAG}. Proceeding with build and deployment."
             }
         }
-
         stage('Build Docker Image') {
+            when {
+                expression {
+                    return env.GIT_TAG == env.TARGET_TAG
+                }
+            }
             steps {
                 script {
+                    // Устанавливаем имя Docker-образа с тегом
+                    env.DOCKER_IMAGE = "staratel/nginx-repo:${env.GIT_TAG}"
+                    echo "Docker Image: ${env.DOCKER_IMAGE}"
+                    // Строим Docker-образ
                     docker.build("${DOCKER_IMAGE}")
                 }
             }
         }
         stage('Push Docker Image') {
+            when {
+                expression {
+                    return env.GIT_TAG == env.TARGET_TAG
+                }
+            }
             steps {
                 script {
+                    // Пушим Docker-образ в Docker Hub
                     docker.withRegistry('https://index.docker.io/v1/', "${DOCKER_CREDENTIALS_ID}") {
                         docker.image("${DOCKER_IMAGE}").push()
                     }
